a5e33c78a856483fe7f4caac68de75dd490ececa
Fortressbeast <24775581+Fortressbeast@users.noreply.github.com>
2019-05-21 11:40:48 -0400

    Small bash to install Wine on Ubuntu for AC

---
+++ WineForAC.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+##ToDo
+##Install wine
+##Auto dl needed files
+##Install additional requirements
+##Install AC
+##Install Decal
+##Install Thwargle
+RED='\033[0;31m'
+WIPE="\033[1m\033[0m"
+
+##Install Wine Staging
+echo "Which version of Ubuntu are you using (Bionic, Cosmic, Disco)?"
+
+read varVersion
+
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+
+if [ "$varVersion" = "Bionic" ]
+	then sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' 
+
+elif [ "$varVersion" = "Cosmic" ]
+	then sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ cosmic main' 
+
+elif [ "$varVersion" = "Disco" ]
+	then sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main' 
+else 
+	echo "You have selected an unknown version, and this istaller is not applicable"
+fi
+
+sudo apt update
+sudo apt install --install-recommends winehq-staging -y
+sudo apt install wine32 -y
+
+cd ~/Downloads
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+sudo cp winetricks /usr/bin
