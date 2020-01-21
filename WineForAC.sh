c3802b70f355985a78d0d3ba170e70c612433718
Fortressbeast <24775581+Fortressbeast@users.noreply.github.com>
2020-01-21 18:10:46 -0500

    added entry for Ermine

--- WineForAC.sh
+++ WineForAC.sh
@@ -10,7 +10,7 @@ RED='\033[0;31m'
 WIPE="\033[1m\033[0m"
 
 ##Install Wine Staging
-echo "Which version of Ubuntu are you using (Bionic, Cosmic, Disco)?"
+echo "Which version of Ubuntu are you using (Bionic, Cosmic, Disco, Ermine)?"
 
 read varVersion
 
@@ -26,6 +26,9 @@ elif [ "$varVersion" = "Cosmic" ]
 
 elif [ "$varVersion" = "Disco" ]
 	then sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main' 
+
+elif [ "$varVersion" = "Ermine" ]
+	then sudo sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main'
 else 
 	echo "You have selected an unknown version, and this istaller is not applicable"
 fi
